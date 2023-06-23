18426aab6928edd6ebc14b8ca66c478c8c67628f
johnny-gill <uint777@gmail.com>
2023-06-23 17:13:04 +0900

    chapter 분리...!!!!!!!!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import Menu from './components/Menu';
-import data from './data/data.json';
+import Menu from './ch5/components/Menu';
+import data from './ch5/data/data.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Menu recipes={data} title="맛있는 조리법" />);
