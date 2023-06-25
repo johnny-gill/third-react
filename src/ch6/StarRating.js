7d15d716e4a25f06b99d8e62d0a778c4f5755d8d
johnny-gill <uint777@gmail.com>
2023-06-25 17:20:02 +0900

    star rating ver2

--- src/ch6/StarRating.js
+++ src/ch6/StarRating.js
@@ -1,12 +1,13 @@
 import { FaStar } from 'react-icons/fa/';
-const StarRating = () => {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />,
-  ];
+
+const createArray = length => [...Array(length)];
+
+const Star = ({ selected = false }) => {
+  return <FaStar color={selected ? 'red' : 'grey'} />;
+};
+
+const StarRating = ({ totalStars = 5 }) => {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
 };
 
 export default StarRating;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client';
 import StarRating from './ch6/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<StarRating />);
+root.render(<StarRating totalStars={10} />);
