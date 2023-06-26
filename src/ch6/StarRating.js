1e572d6fdd368d3d3b540d0ce6318bcf75f344f3
johnny-gill <uint777@gmail.com>
2023-06-26 22:59:33 +0900

    star rating ver3

--- src/ch6/StarRating.js
+++ src/ch6/StarRating.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { FaStar } from 'react-icons/fa/';
 
-const createArray = length => [...Array(length)];
+const createArray = (length) => [...Array(length)];
 
-const Star = ({ selected = false }) => {
-  return <FaStar color={selected ? 'red' : 'grey'} />;
+
+const Star = ({ selected = false, onSelect = f => f }) => {
+  return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />;
 };
 
 const StarRating = ({ totalStars = 5 }) => {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)}/>
+      ))}
+      <p>
+        {selectedStars} / {totalStars}
+      </p>
+    </>
+  );
 };
 
 export default StarRating;
