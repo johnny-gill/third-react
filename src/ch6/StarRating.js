503896de07187e942bfefeb2b2230dc0ef73a128
johnny-gill <uint777@gmail.com>
2023-06-27 16:56:51 +0900

    star rating ver3

--- src/ch6/StarRating.js
+++ src/ch6/StarRating.js
@@ -3,22 +3,26 @@ import { FaStar } from 'react-icons/fa/';
 
 const createArray = (length) => [...Array(length)];
 
-
-const Star = ({ selected = false, onSelect = f => f }) => {
+const Star = ({ selected = false, onSelect = (f) => f }) => {
   return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />;
 };
 
-const StarRating = ({ totalStars = 5 }) => {
+const StarRating = ({ totalStars = 5, style = {}, ...props }) => {
+  console.log(props);
   const [selectedStars, setSelectedStars] = useState(0);
   return (
-    <>
+    <div style={{ padding: '5px', ...style }} {...props}>
       {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)}/>
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} / {totalStars}
       </p>
-    </>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import ReactDOM from 'react-dom/client';
 import StarRating from './ch6/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<StarRating totalStars={10} />);
+root.render(
+  <StarRating
+    totalStars={10}
+    style={{ backgroundColor: 'lightblue' }}
+    onDoubleClick={() => alert('double click')}
+  />
+);
