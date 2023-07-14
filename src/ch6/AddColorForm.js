8a0f983c5abc372332fde1657cda974c367605b7
johnny gill <uint777@gmail.com>
2023-07-14 15:18:33 +0900

    refactor : useState - controlled component

--- src/ch6/AddColorForm.js
+++ src/ch6/AddColorForm.js
@@ -1,27 +1,33 @@
-import { useRef } from 'react';
+import { useState } from "react";
 
-const AddColorForm = ({onNewColor = (f) => f}) => {
-  const txtTitle = useRef();
-  const hexColor = useRef();
+const AddColorForm = ({ onNewColor = (f) => f }) => {
+  console.log('AddColorForm');
+  
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
 
-  const onSubmitHandler = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
     onNewColor(title, color);
-    txtTitle.current.value = '';
-    hexColor.current.value = '';
+    setTitle("");
+    setColor("#000000");
   };
 
   return (
-    <form onSubmit={onSubmitHandler}>
+    <form onSubmit={handleSubmit}>
       <input
-        ref={txtTitle}
         type="text"
         placeholder="칼라타이틀 입력"
+        value={title}
         required
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <input ref={hexColor} type="color" required />
+      <input
+        type="color"
+        value={color}
+        required
+        onChange={(e) => setColor(e.target.value)}
+      />
       <button>추가</button>
     </form>
   );
