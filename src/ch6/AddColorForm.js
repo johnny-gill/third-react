c7fcda643bf76ee2f2032109f8d675e2ae4a9624
johnny gill <uint777@gmail.com>
2023-07-14 15:55:33 +0900

    refactor : Add custom hook

--- src/ch6/AddColorForm.js
+++ src/ch6/AddColorForm.js
@@ -1,32 +1,30 @@
-import { useState } from "react";
+import { useInput } from "./hooks";
 
 const AddColorForm = ({ onNewColor = (f) => f }) => {
-  console.log('AddColorForm');
-  
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  console.log("AddColorForm");
+
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onNewColor(title, color);
-    setTitle("");
-    setColor("#000000");
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder="칼라타이틀 입력"
-        value={title}
+        placeholder="insert color title"
         required
-        onChange={(e) => setTitle(e.target.value)}
+        {...titleProps}
       />
       <input
         type="color"
-        value={color}
         required
-        onChange={(e) => setColor(e.target.value)}
+        {...colorProps}
       />
       <button>추가</button>
     </form>
