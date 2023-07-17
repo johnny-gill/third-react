453cb1bb60cb5515956d00a3fb0a60c3108e998b
johnny gill <uint777@gmail.com>
2023-07-17 13:50:34 +0900

    Feature : Adding a color to state

--- src/ch6/App.js
+++ src/ch6/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import colorData from './color-data.json';
-import ColorList from './ColorList';
-import AddColorForm from './AddColorForm';
-
+import { useState } from "react";
+import colorData from "./color-data.json";
+import ColorList from "./ColorList";
+import AddColorForm from "./AddColorForm";
+import { v4 } from "uuid";
 const App = () => {
   const [colors, setColors] = useState(colorData);
 
@@ -20,7 +20,12 @@ const App = () => {
 
   return (
     <>
-      <AddColorForm onNewColor={(title, color) => alert(`${title}=${color}`)} />
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [...colors, { id: v4(), rating: 0, title, color }];
+          setColors(newColors);
+        }}
+      />
       <ColorList
         colors={colors}
         onRemoveColor={removeColor}
