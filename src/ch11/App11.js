bead6dd295934e1ba560ecb85f1eb58ceda1a004
johnny gill <uint777@gmail.com>
2023-07-28 16:30:44 +0900

    Refactor: route subpage

--- src/ch11/App11.js
+++ src/ch11/App11.js
@@ -1,15 +1,29 @@
 import { Route, Routes } from "react-router";
-import { About, Contact, Error, Events, Home, Products } from "./pages";
+import {
+  About,
+  Contact,
+  Error,
+  Events,
+  History,
+  Home,
+  Location,
+  Products,
+  Services,
+} from "./pages";
 
 const App11 = () => (
   <div>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/*" element={<Error />} />
+      <Route path="about" element={<About />}>
+        <Route path="services" element={<Services />} />
+        <Route path="history" element={<History />} />
+        <Route path="location" element={<Location />} />
+      </Route>
+      <Route path="events" element={<Events />} />
+      <Route path="products" element={<Products />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   </div>
 );
--- src/ch11/pages.js
+++ src/ch11/pages.js
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export const Home = () => (
   <div>
@@ -15,6 +15,7 @@ export const Home = () => (
 export const About = () => (
   <div>
     <h1>[회사소개]</h1>
+    <Outlet />
   </div>
 );
 
@@ -38,10 +39,109 @@ export const Contact = () => (
 
 export const Error = () => {
   const location = useLocation();
-  console.log(location);
   return (
     <div>
       <h1>Resource not founds at {location.pathname}</h1>
     </div>
   );
 };
+
+export const Services = () => (
+  <section className="services">
+    <h2>Our Services</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+      nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+      tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+      nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+      per inceptos himenaeos. Curabitur sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+      risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+      ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+      tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos.
+    </p>
+  </section>
+);
+
+export const Location = () => (
+  <section className="location">
+    <h2>Our Location</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam.
+    </p>
+    <p>
+      Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+      Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora
+      torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales
+      ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+      risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+      ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+      tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos.
+    </p>
+  </section>
+);
+export const Company = () => (
+  <section className="company">
+    <h2>About the Company</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+      nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+      tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+      nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+      per inceptos himenaeos. Curabitur sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+      risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+      ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+      tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos.
+    </p>
+  </section>
+);
+export const History = () => (
+  <section className="history">
+    <h2>Our History</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+      nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+      tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+      nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+      per inceptos himenaeos. Curabitur sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor.
+    </p>
+    <p>
+      {" "}
+      Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce
+      ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus,
+      ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat
+      condimentum velit. Class aptent taciti sociosqu ad litora torquent per
+      conubia nostra, per inceptos himenaeos.
+    </p>
+  </section>
+);
